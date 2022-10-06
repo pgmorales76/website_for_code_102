8d4883279cbe9e564f74f89773ad07a6228f7f8d
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 04:56:36 -0400

    Removed js code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,3 @@
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
 
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Thanks for dropping by, ${myName}!`;
-  }
-}
-
-myButton.onclick = () => {
-  setUserName();
-};
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
