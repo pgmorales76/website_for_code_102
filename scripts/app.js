284c5286298205f979cceee16f2aac3e34a61768
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 14:36:18 -0400

    Removed prompt

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,3 @@
-let favorite_author = prompt("Who is your favorite author?");
-// console.log(favorite_author);
-
-if (favorite_author.toLowerCase() == "ayn rand") {
-    console.log("Me, too!!!")
-} else {
-    console.log("Great choice!")
-}
 
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/app.js */
