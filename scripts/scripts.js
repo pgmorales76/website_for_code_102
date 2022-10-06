87b8eb46b21fe067fca38bc99c9eb61e616d8685
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 04:37:18 -0400

    Removed an event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,3 @@
-document.querySelector("html").addEventListener("click", function () {
-  alert("Ouch! Stop poking me!");
-});
-
 const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
