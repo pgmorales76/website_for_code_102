6e67a99b7bcd4d2734fb659c6759c81b8968fc5c
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 02:48:34 -0400

    Add link for vs code and browser

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,7 @@
 document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
+
   
+
+/* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
