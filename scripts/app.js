1a8bdcfe7b9e759bfbf4e496bf96d9470912e5c8
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 07:13:17 -0400

    Update to prompt

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 let favorite_author = prompt("Who is your favorite author?");
 // console.log(favorite_author);
 
-if (favorite_author.toLowerCase == "ayn rand") {
+if (favorite_author.toLowerCase() == "ayn rand") {
     console.log("Me, too!!!")
 } /* else {
         console.log("Great choice!")
 }*/
+
+
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/app.js */
