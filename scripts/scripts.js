abf6ecaf0ea23c8c5edbe0c25c34945db9decd12
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 04:50:49 -0400

    Added a welcome message

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,18 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
 
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Thanks for dropping by, ${myName}!`;
+  }
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
