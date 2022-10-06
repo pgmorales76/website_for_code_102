92786f3c2c935e0e9b8f1a188616965c54941e39
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 04:41:23 -0400

    Remove an event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,3 @@
-const myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "https://pgmorales76.github.io/website_for_code_102/images/firefox_logo_developer_edition.png") {
-    myImage.setAttribute("src", "https://pgmorales76.github.io/website_for_code_102/images/firefox_logo_nightly.png");
-  } else {
-    myImage.setAttribute("src", "https://pgmorales76.github.io/website_for_code_102/images/firefox_logo_developer_edition.png");
-  }
-};
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
