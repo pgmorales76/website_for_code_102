5b260b751802f7fa0f342790e3cdd05fae0c253e
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 03:14:45 -0400

    Added an image changer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,17 @@ document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
 
+  const myImage = document.querySelector("img");
+
+  myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+      myImage.setAttribute("src", "/Users/peterg.morales/Documents/code_fellows/projects/courses/102/website_for_code_102/website_for_code_102/images/firefox_logo_developer_edition.png");
+    } else {
+      myImage.setAttribute("src", "/Users/peterg.morales/Documents/code_fellows/projects/courses/102/website_for_code_102/website_for_code_102/images/firefox_logo_nightly.png");
+    }
+  };
+  
   
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
