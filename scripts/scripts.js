2426b03711b90a60f5f38063635744dafdc756a0
Peter G. Morales <pgmorales76@gmail.com>
2022-10-06 04:05:29 -0400

    Changed event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,7 @@
 document.querySelector("html").addEventListener("click", function () {
-    alert("Watch the image change!");
-  });
+  alert("Ouch! Stop poking me!");
+});
 
-  const myImage = document.querySelector("img");
 
-  myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "/Users/peterg.morales/Documents/code_fellows/projects/courses/102/website_for_code_102/website_for_code_102/images/firefox_logo_developer_edition.png") {
-      myImage.setAttribute("src", "/Users/peterg.morales/Documents/code_fellows/projects/courses/102/website_for_code_102/website_for_code_102/images/firefox_logo_nightly.png");
-    } else {
-      myImage.setAttribute("src", "/Users/peterg.morales/Documents/code_fellows/projects/courses/102/website_for_code_102/website_for_code_102/images/firefox_logo_developer_edition.png");
-    }
-  };
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/scripts.js */
