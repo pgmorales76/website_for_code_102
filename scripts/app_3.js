af5ccbf144d8eb5afb3adcf2f795d5562c2ce1e3
Peter G. Morales <pgmorales76@gmail.com>
2022-10-07 03:06:03 -0400

    Deleted two JS files

--- scripts/app_3.js
+++
@@ -1,7 +0,0 @@
-if (confirm("Shall I print Hello World?")) {
-    document.write("Hello World");
-} else {
-    document.write("OK, I won't print it.");
-}
-
-/* https://pgmorales76.github.io/website_for_code_102/scripts/app_3.js */
