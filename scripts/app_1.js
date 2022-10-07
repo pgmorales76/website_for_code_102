be33b0609b0dc34a6c557c92e16dea2b27e22a42
Peter G. Morales <pgmorales76@gmail.com>
2022-10-07 16:17:30 -0400

    Added data validation to js file

--- scripts/app_1.js
+++ scripts/app_1.js
@@ -2,6 +2,10 @@ function how_many_books() {
         let number = prompt("How many books have you read this year? (Use numbers, please!)");
         let message;
 
+        if (x == "") {
+                alert("That's not a vaild number, silly! C'mon, tell me!");
+                return false;
+        }
         if (number <= 2) {
                 message = "Seriously? You gotta stop binge-watching!!!";
         } else if (number <= 10) {
@@ -20,6 +24,10 @@ function favorite_genre() {
         let genre = prompt("Is your favorite genre mystery, science fiction, or horror?");
         let message;
 
+        if (x == "") {
+                alert("That's not a genre, silly! C'mon, tell me!");
+                return false;
+        }
         if (genre.toLowerCase() == "mystery") {
                 document.write("What a mysterious answer!");
         } else if (author.toLowerCase() == "science fiction") {
@@ -36,6 +44,10 @@ function favorite_author() {
         let author = prompt("Is your favorite author Ayn Rand, Michael Crichton, or Walter Tevis?");
         let message;
 
+        if (x == "") {
+                alert("Who is that? C'mon, tell me!");
+                return false;
+        }
         if (author.toLowerCase() == "ayn rand") {
                 document.write("Who is John Galt?");
         } else if (author.toLowerCase() == "michael crichton") {
