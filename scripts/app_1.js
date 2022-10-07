b0d6a484f923373e3a4c1a10ed3136712750a414
Peter G. Morales <pgmorales76@gmail.com>
2022-10-07 03:04:57 -0400

    Put all JS code into one file

--- scripts/app_1.js
+++ scripts/app_1.js
@@ -1,4 +1,21 @@
 var name = prompt("Your name:", "");
-        document.write("Hello ", name);
+document.write("Hello ", name);
+
+let favorite_author = prompt("Who is your favorite author?");
+// console.log(favorite_author);
+
+
+
+if (favorite_author.toLowerCase() == "ayn rand") {
+        document.write("Me, too!!!")
+} else {
+        document.write("Great choice!")
+}
+
+if (confirm("Shall I print Hello World?")) {
+        document.write("Hello World");
+} else {
+        document.write("OK, I won't print it.");
+}
 
 /* https://pgmorales76.github.io/website_for_code_102/scripts/app_1.js */
